89cc816176d87c5c82a392c62edaef2ce879c175
Vaidas Mar <vaidas1@inbox.lt>
2022-07-12 22:41:32 +0300

    JAVASCRIPT ADDED

---
+++ JS DARBAS 1/script.js
@@ -0,0 +1,142 @@
+                                        // Uzduotis 1
+
+// const legalAge = 20;
+// let clienAge = window.prompt("Koks tavo amziu:");
+
+// if (legalAge <= clienAge) {
+//     alert("Gali vairuoti!");
+// } else {
+//     alert("Negali vairuoti!");
+// }
+
+                                            // Uzduotis 2 
+
+// const manoVardas= "Vaidas";
+// if (manoVardas.length > 6) {
+//     alert("Vardas per ilgas!");
+// }
+
+
+                                            // Uzduotis 3 
+
+// const manoAmzius = 4;
+// if (manoAmzius > 100 || manoAmzius < 0){
+//     alert("Invalid Age");
+// } 
+// else if (manoAmzius > 1 && manoAmzius < 18) {
+//     alert("Child!");
+// } else{
+//     alert("Adult");
+// }
+
+// Uzduotis 4
+
+// const car = "Skoda";
+// if (car === 'VW' || car === 'Audi' || car === 'Bentley' || car === 'Bugatti' || car === 'Lamborghini' || car === 'Porsche') {
+//     alert("VW Group");
+//   } else if (car === 'BMW' || car === 'Mini' || car === 'Rolls-Royce') {
+//     alert('BMW Group');
+//   } else {
+//     alert('Nei vienam');
+//   }
+
+// Uzduotis 5 Switch
+
+// const car = "Skoda";
+// switch(car){
+//     case "VW":
+//     case "Audi":
+//     case "Bentley":
+//     case "Bugatti":
+//     case "Lamborghini":
+//     case "Porsche":
+//             alert("VW Group")
+//             break;
+//     case "BMW":
+//     case "Mini":
+//     case "Rolls-Royce":
+//             alert("BMW Group")
+//             break;  
+//     default: alert("Ne vienam");
+// } 
+
+// Uzduotis 6 Switch
+
+// const userInput = 'Aviete';
+// switch (userInput) {
+//   case 'Bulve':
+//   case 'Kopustas':
+//   case 'Cesnakas':
+//   case 'Svogūnas':
+//   case 'Morka':
+//     alert("Darzove");
+//     break;
+//   case 'Obuolys':
+//   case 'Kriause':
+//   case 'Aviete':
+//   case 'Braske':
+//   case 'Vynuoge':
+//     alert('Vaisius');
+//     break;
+//   default:
+//     alert("Nei darzove, nei vaisius")
+// }
+
+
+// Uzduotis 7 Ternary Operatorius
+
+
+// const manoVardas = "Vaidas"
+// alert(manoVardas.length > 6 ? "Ilgas vardas" : "Trumpas vardas");
+
+// const clienAge = 122;
+// const legalAge = 20;
+// clienAge < 0 || clienAge > 120 ? alert("Invalid Age!") : clienAge >= legalAge ? alert("Galima vaiduoti!") : alert("Negalima vairuoti!");
+
+// Uzduotis 8 Ivadas i ciklus
+
+// const manoVardas = "Vaidas";
+// const count = 10;
+// for (let i = 0; i < count; i++){
+//     console.log(`No ${i}. ${manoVardas}`);
+// }
+
+
+// Uzduotis 9 While ciklai
+
+
+// const manoVardas = "Vaidas";
+// let i = 0;
+// while (i < 3) {
+//     console.log(manoVardas);
+//     i++;
+// }
+
+// let combo = "";
+// const manoVardas = "Vaidas";
+// let times = 5;
+// do {
+//   combo += manoVardas;
+//   times--;
+// } while (times > 0);
+// console.log(combo);
+
+
+                                            // Uzduotis 10 MATH
+
+
+// alert(Math.cos(90));
+// alert(Math.floor(Math.random() * 5 + 1));
+
+// for (i = 0; i < 10; i++){
+// console.log(Math.floor(Math.random() * (12 - 5 + 1)) + 5);}
+
+// const randomNumber = Math.floor(Math.random() * 5) + 1;
+// if (randomNumber === 1) {
+//   alert("You won");
+// } else {
+//   alert("Try again next time");
+// }
+
+
+                                            // Uzduotis 11 DOM
